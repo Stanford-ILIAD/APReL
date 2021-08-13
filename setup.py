533ab753f2b26f354eb6b8f932513ede96050b07
erdem-biyik <ebiyik@stanford.edu>
2021-08-13 04:05:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     lines = f.readlines()
 
 # remove images from README
